ff239e48c36234fd71c8e85458dda209e8598cdb
shailus1029 <shailus1029@gmail.com>
2021-06-19 09:57:05 +0530

    server created using http and nodemon adde

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const http = require("http");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
-
+//initilizing our express app
 const app = express();
+app.use(express.json());
 
 app.get("/server-status", (req, res) => {
     res.send("Server is running properly");
 })
 
-app.listen(PORT, () => {
+//creating http server
+const server = http.createServer(app);
+
+//defining port
+const PORT = 5000;
+
+//listening to the port
+server.listen(PORT, () => {
     console.log("Server is running on port:", PORT);
 });
 
